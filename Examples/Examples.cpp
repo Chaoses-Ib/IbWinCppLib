13f4f289bd098ceacc0d720018e32b618e70f86a
Chaoses Ib <Chaos-es@outlook.com>
2021-02-10 05:02:54 +0800

    Major modification

---
+++ Examples/Examples.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "..\WinCppLib\include\WinCppLib.hpp"
+
+using namespace std;
+using namespace ib;
+
+int main()
+{
+    //auto a = wzstring(L"abc"s);
+    cout << (void*)ModuleFactory::Load(L"ntdll.dll")->base << endl;
+    cout << "Hello World!\n";
+}
